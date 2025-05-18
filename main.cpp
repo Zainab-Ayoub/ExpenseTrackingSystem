c1c482c233a1e5151fff28a2fa254c6f84f5f674
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-05-19 00:02:34 +0500

    structure for project

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct Expense{
+
+};
+
+struct Category{
+
+};
+
+Category* findCategory(Category* head, string name){
+
+}
+
+void addCategory(){
+
+}
+
+void addExpense(){
+
+}
+
+void viewExpense(){
+
+}
+
+void updateExpense(){
+
+}
+
+void deleteExpense(){
+
+}
+
+void mostExpensiveExpense(){
+
+}
+
+void showAllCategories(){
+
+}
+
+void cleanup(){
+    
+}
+
+int main(){
+
+}
